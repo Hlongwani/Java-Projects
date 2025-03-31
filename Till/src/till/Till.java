810cc07755f9bcf7e7f6d4fc57da1a4bef162d69
Hlongwani <kaybeejaybee2002@gmail.com>
2025-03-31 12:02:45 +0200

    Add files via upload

---
+++ Till/src/till/Till.java
@@ -0,0 +1,61 @@
+package till;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Till extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    
+    public static void main(String[] args) {
+      //  launch(args);
+        
+        Socket socket;
+        DataOutputStream dataOut;
+        
+        try {
+            
+            socket =new Socket("localhost",9090);
+            dataOut= new DataOutputStream(socket.getOutputStream());
+            dataOut.writeUTF("Nkavelo");
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Till.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+    
+}
